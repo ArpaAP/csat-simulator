f00b5a2df4c03fc7d6681edf57310c5d908159dd
알파 <arpa1705@gmail.com>
2023-05-11 16:55:19 +0900

    fix: 오디오 파일 디렉터리 변경

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.(mp3)$/,
-      use: {
-        loader: "file-loader",
-        options: {
-          name: "[name].[ext]",
-          publicPath: `/_next/static/sounds/`,
-          outputPath: `${options.isServer ? "../" : "src/"}static/sounds/`,
-        },
-      },
-    });
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -1,35 +1,3 @@
-import A001 from "../static/sounds/001.mp3";
-import A002 from "../static/sounds/002.mp3";
-import A003 from "../static/sounds/003.mp3";
-import A004 from "../static/sounds/004.mp3";
-import A005 from "../static/sounds/005.mp3";
-import A006 from "../static/sounds/006.mp3";
-import A007 from "../static/sounds/007.mp3";
-import A008 from "../static/sounds/008.mp3";
-import A009 from "../static/sounds/009.mp3";
-import A010 from "../static/sounds/010.mp3";
-import A011 from "../static/sounds/011.mp3";
-import A012 from "../static/sounds/012.mp3";
-import A013 from "../static/sounds/013.mp3";
-import A014 from "../static/sounds/014.mp3";
-import A015 from "../static/sounds/015.mp3";
-import A016 from "../static/sounds/016.mp3";
-import A017 from "../static/sounds/017.mp3";
-import A018 from "../static/sounds/018.mp3";
-import A019 from "../static/sounds/019.mp3";
-import A020 from "../static/sounds/020.mp3";
-import A021 from "../static/sounds/021.mp3";
-import A022 from "../static/sounds/022.mp3";
-import A023 from "../static/sounds/023.mp3";
-import A024 from "../static/sounds/024.mp3";
-import A025 from "../static/sounds/025.mp3";
-import A026 from "../static/sounds/026.mp3";
-import A027 from "../static/sounds/027.mp3";
-import A028 from "../static/sounds/028.mp3";
-import A029 from "../static/sounds/029.mp3";
-import A030 from "../static/sounds/030.mp3";
-import A031 from "../static/sounds/031.mp3";
-
 const TIMELINE: {
   time: string;
   description: string;
@@ -42,112 +10,112 @@ const TIMELINE: {
     description: "1교시 입실준비",
     short: "입실준비",
     duration: 5,
-    audio: A001,
+    audio: "/sounds/001.mp3",
   },
   {
     time: "0810",
     description: "1교시 입실완료",
     short: "입실완료",
     duration: 15,
-    audio: A002,
+    audio: "/sounds/002.mp3",
   },
   {
     time: "0825",
     description: "1교시 국어 예비령",
     short: "예비령",
     duration: 10,
-    audio: A003,
+    audio: "/sounds/003.mp3",
   },
   {
     time: "0835",
     description: "1교시 국어 준비령",
     short: "준비령",
     duration: 5,
-    audio: A004,
+    audio: "/sounds/004.mp3",
   },
   {
     time: "0840",
     description: "1교시 국어 본령",
     short: "본령",
     duration: 70,
-    audio: A005,
+    audio: "/sounds/005.mp3",
   },
   {
     time: "0950",
     description: "1교시 국어 종료 10분전",
     short: "10분전",
     duration: 10,
-    audio: A006,
+    audio: "/sounds/006.mp3",
   },
   {
     time: "1000",
     description: "1교시 종료령",
     short: "종료령",
     duration: 15,
-    audio: A007,
+    audio: "/sounds/007.mp3",
   },
   {
     time: "1015",
     description: "2교시 입실",
     short: "입실",
     duration: 5,
-    audio: A008,
+    audio: "/sounds/008.mp3",
   },
   {
     time: "1020",
     description: "2교시 수학 예비령",
     short: "예비령",
     duration: 5,
-    audio: A009,
+    audio: "/sounds/009.mp3",
   },
   {
     time: "1025",
     description: "2교시 수학 준비령",
     short: "준비령",
     duration: 5,
-    audio: A010,
+    audio: "/sounds/010.mp3",
   },
   {
     time: "1030",
     description: "2교시 수학 본령",
     short: "본령",
     duration: 90,
-    audio: A011,
+    audio: "/sounds/011.mp3",
   },
   {
     time: "1200",
     description: "2교시 수학 종료 10분전",
     short: "10분전",
     duration: 10,
-    audio: A012,
+    audio: "/sounds/012.mp3",
   },
   {
     time: "1210",
     description: "2교시 종료령",
     short: "종료령",
     duration: 45,
-    audio: A013,
+    audio: "/sounds/013.mp3",
   },
   {
     time: "1255",
     description: "3교시 입실",
     short: "입실",
     duration: 5,
-    audio: A014,
+    audio: "/sounds/014.mp3",
   },
   {
     time: "1300",
     description: "3교시 영어 예비령",
     short: "예비령",
     duration: 5,
-    audio: A015,
+    audio: "/sounds/015.mp3",
   },
   {
     time: "1305",
     description: "3교시 영어 준비령",
     short: "준비령",
     duration: 5,
-    audio: A016,
+    audio: "/sounds/016.mp3",
   },
   {
     time: "1310",
@@ -161,105 +129,105 @@ const TIMELINE: {
     description: "3교시 영어 종료 10분전",
     short: "10분전",
     duration: 10,
-    audio: A017,
+    audio: "/sounds/017.mp3",
   },
   {
     time: "1420",
     description: "3교시 종료령",
     short: "종료령",
     duration: 15,
-    audio: A018,
+    audio: "/sounds/018.mp3",
   },
   {
     time: "1435",
     description: "4교시 한국사 입실",
     short: "입실",
     duration: 5,
-    audio: A019,
+    audio: "/sounds/019.mp3",
   },
   {
     time: "1440",
     description: "4교시 한국사 예비령",
     short: "예비령",
     duration: 5,
-    audio: A020,
+    audio: "/sounds/020.mp3",
   },
   {
     time: "1445",
     description: "4교시 한국사 준비령",
     short: "준비령",
     duration: 5,
-    audio: A021,
+    audio: "/sounds/021.mp3",
   },
   {
     time: "1450",
     description: "4교시 한국사 본령",
     short: "본령",
     duration: 25,
-    audio: A022,
+    audio: "/sounds/022.mp3",
   },
   {
     time: "1515",
     description: "4교시 한국사 종료 5분전",
     short: "5분전",
     duration: 5,
-    audio: A023,
+    audio: "/sounds/023.mp3",
   },
   {
     time: "1520",
     description: "4교시 한국사 종료령",
     short: "종료령",
     duration: 5,
-    audio: A024,
+    audio: "/sounds/024.mp3",
   },
   {
     time: "1525",
     description: "4교시 탐구 준비령",
     short: "준비령",
     duration: 5,
-    audio: A025,
+    audio: "/sounds/025.mp3",
   },
   {
     time: "1530",
     description: "4교시 탐구 제1선택 본령",
     short: "본령",
     duration: 25,
-    audio: A026,
+    audio: "/sounds/026.mp3",
   },
   {
     time: "1555",
     description: "4교시 탐구 제1선택 종료 5분전",
     short: "5분전",
     duration: 5,
-    audio: A027,
+    audio: "/sounds/027.mp3",
   },
   {
     time: "1600",
     description: "4교시 탐구 제1선택 종료령",
     short: "종료령",
     duration: 2,
-    audio: A028,
+    audio: "/sounds/028.mp3",
   },
   {
     time: "1602",
     description: "4교시 탐구 제2선택 본령",
     short: "본령",
     duration: 25,
-    audio: A029,
+    audio: "/sounds/029.mp3",
   },
   {
     time: "1627",
     description: "4교시 탐구 제2선택 종료 5분전",
     short: "5분전",
     duration: 5,
-    audio: A030,
+    audio: "/sounds/030.mp3",
   },
   {
     time: "1632",
     description: "4교시 탐구 제2선택 종료령",
     short: "종료령",
     duration: 0,
-    audio: A031,
+    audio: "/sounds/031.mp3",
   },
 ];
 
